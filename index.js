dd0f7059a537927c01bf398a39ffcfd855b8e35b
shahedsaadi <shahedsaadi8@gmail.com>
2024-05-08 21:28:46 +0300

    edit index.js

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const db = new pg.Client({
 
 db.connect();
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASS:", process.env.DB_PASS);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const app = express();
 const port = process.env.PORT || 3000;
   
